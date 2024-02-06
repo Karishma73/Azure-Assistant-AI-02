d57b73918b163e40a1fcbb9129222736fb0c69f3
Karishma73 <60409314+Karishma73@users.noreply.github.com>
2024-02-06 18:17:50 +0000

    Update end2end.py
    pytk==0.0.2.1

--- end2end.py
+++ end2end.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import nltk
 #nltk.download()
-import tkinter
+import tkinter as tk
 #from tkinter.ttk import *
 from pandasai import SmartDatalake
 from pandasai.llm import AzureOpenAI
