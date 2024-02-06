85f89b9f5a303b233c92e6ad6f48f8c751db7276
Karishma73 <60409314+Karishma73@users.noreply.github.com>
2024-02-06 17:52:14 +0000

    Update end2end.py
    added capital T for tkinter

--- end2end.py
+++ end2end.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import nltk
 #nltk.download()
-import tkinter as tk
+import Tkinter as tk
 from pandasai import SmartDatalake
 from pandasai.llm import AzureOpenAI
 import streamlit as st
